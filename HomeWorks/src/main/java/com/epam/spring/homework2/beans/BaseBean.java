27e7c1b256823815c5d5464c62b7b50ecaaf0321
Panchenko, Kostyantyn [External] <Kostyantyn.Panchenko@externals.adidas.com>
2022-01-30 22:42:28 +0200

    HW_2 - Refactor config to inject beans' values in the method parameters

---
+++ HomeWorks/src/main/java/com/epam/spring/homework2/beans/BaseBean.java
@@ -0,0 +1,2 @@
+package com.epam.spring.homework2.beans;public class BaseBean {
+}
--- HomeWorks/src/main/java/com/epam/spring/homework2/config/BeanConfig.java
+++ HomeWorks/src/main/java/com/epam/spring/homework2/config/BeanConfig.java
@@ -4,45 +4,31 @@ import com.epam.spring.homework2.beans.BeanB;
 import com.epam.spring.homework2.beans.BeanC;
 import com.epam.spring.homework2.beans.BeanD;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Import(OtherConfig.class)
 @PropertySource("classpath:application.properties")
 public class BeanConfig {
 
-    @Value("${beanB.name}")
-    private String beanBName;
-
-    @Value("${beanB.value}")
-    private int beanBValue;
-
-    @Value("${beanC.name}")
-    private String beanCName;
-
-    @Value("${beanC.value}")
-    private int beanCValue;
-
-    @Value("${beanD.name}")
-    private String beanDName;
-
-    @Value("${beanD.value}")
-    private int beanDValue;
-
     @Bean(initMethod = "initMethod", destroyMethod = "destroyMethod")
     @DependsOn(value = "beanD")
-    public BeanB beanB() {
-        return new BeanB(beanBName, beanBValue);
+    public BeanB beanB(@Value("${beanB.name}") final String name, @Value("${beanB.value}") final int value) {
+        return new BeanB(name, value);
     }
 
     @Bean(initMethod = "initMethod", destroyMethod = "destroyMethod")
     @DependsOn({"beanD", "beanB"})
-    public BeanC beanC() {
-        return new BeanC(beanCName, beanCValue);
+    public BeanC beanC(@Value("${beanC.name}") final String name, @Value("${beanC.value}") final int value) {
+        return new BeanC(name, value);
     }
 
     @Bean(initMethod = "initMethod", destroyMethod = "destroyMethod")
-    public BeanD beanD() {
-        return new BeanD(beanDName, beanDValue);
+    public BeanD beanD(@Value("${beanD.name}") final String name, @Value("${beanD.value}") final int value) {
+        return new BeanD(name, value);
     }
 }
